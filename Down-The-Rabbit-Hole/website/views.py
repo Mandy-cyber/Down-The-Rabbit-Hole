5982849a73f139c48ebd64a029f91a51fd5b841c
Mandy-cyber <92461362+PeriodPrideJa@users.noreply.github.com>
2022-05-20 18:48:21 -0500

    Co-authored-by: annabellechung <annabellechung@users.noreply.github.com> Co-authored-by: juzsarah <juzsarah@users.noreply.github.com>

--- Down-The-Rabbit-Hole/website/views.py
+++ Down-The-Rabbit-Hole/website/views.py
@@ -5,3 +5,23 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def landing():
     return render_template("landing.html")
+
+@views.route('/options', methods=['GET', 'POST'])
+def theme_options():
+    return render_template("options.html")
+
+@views.route('/history', methods=['GET', 'POST'])
+def history():
+    return render_template("history.html")
+
+@views.route('/art', methods=['GET', 'POST'])
+def art():
+    return render_template("art.html")
+
+@views.route('/music', methods=['GET', 'POST'])
+def music():
+    return render_template("music.html")
+
+@views.route('/tech', methods=['GET', 'POST'])
+def tech():
+    return render_template("tech.html")
