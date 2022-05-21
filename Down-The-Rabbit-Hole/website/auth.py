5dd1f25b655b8592be915d4598eb3fb97b7b91ac
Mandy-cyber <92461362+PeriodPrideJa@users.noreply.github.com>
2022-05-21 01:04:18 -0500

    setting up auth.py functions and routes, changing existing routes to login required

---
+++ Down-The-Rabbit-Hole/website/auth.py
@@ -0,0 +1,69 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User, Snippet
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
+
+auth = Blueprint('auth', __name__) 
+
+#----------------------------------------------------------------------------------------------------------------------------#
+#LOGIN STUFF
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST': 
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("You're in!", category='success') #TODO ask Belle to draw our own flashes instead of having to use basic Flask ones
+                login_user(user, remember=True) 
+                return redirect(url_for('views.landing'))
+            else:
+                flash('Oopsie, wrong username or password, try again.', category='error')
+        else:
+            flash('Oopsie, wrong username or password, try again.', category='error')
+    return render_template("login.html", user=current_user)
+
+#----------------------------------------------------------------------------------------------------------------------------#
+#LOGOUT STUFF
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+#----------------------------------------------------------------------------------------------------------------------------#
+#SIGN UP STUFF
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password1')
+        confirm_password = request.form.get('password2')
+        #DOING SOME VALIDATION CHECKS
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash('Hmm, it seems like this username is already taken!', category='error')
+        else:
+            if len(email) < 4:
+                flash('Invalid email address', category='error')
+            elif password != confirm_password:
+                flash("Your passwords don't match! Try again.", category='error')
+            elif len(password) < 7:
+                flash('Nuh uh, you need a stronger password!', category='error')
+            else:
+                #add user to database
+                new_user = User(username=username, email=email, password=generate_password_hash(password, method='sha256'))
+                db.session.add(new_user)
+                db.session.commit()
+                flash('Account created successfully! Welcome to the rabbit hold <3', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.landing'))
+    return render_template("signup.html", user=current_user)
+
+#----------------------------------------------------------------------------------------------------------------------------#
--- Down-The-Rabbit-Hole/website/views.py
+++ Down-The-Rabbit-Hole/website/views.py
@@ -1,31 +1,39 @@
-from flask import Blueprint, jsonify, render_template, flash, request, jsonify
+from flask import Blueprint, jsonify, render_template, flash, request
+from flask_login import login_user, login_required, logout_user, current_user
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
+@login_required
 def landing():
     return render_template("landing.html")
 
 @views.route('/message')
+@login_required
 def see_message():
     return render_template("message.html")
 
 @views.route('/options', methods=['GET', 'POST'])
+@login_required
 def theme_options():
     return render_template("options.html")
 
 @views.route('/history', methods=['GET', 'POST'])
+@login_required
 def history():
     return render_template("history.html")
 
 @views.route('/art', methods=['GET', 'POST'])
+@login_required
 def art():
     return render_template("art.html")
 
 @views.route('/music', methods=['GET', 'POST'])
+@login_required
 def music():
     return render_template("music.html")
 
 @views.route('/tech', methods=['GET', 'POST'])
+@login_required
 def tech():
     return render_template("tech.html")
