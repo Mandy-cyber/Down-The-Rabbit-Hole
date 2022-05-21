eaa6817de4e10e647903b6b06ce85268d35bd7fe
Mandy-cyber <92461362+PeriodPrideJa@users.noreply.github.com>
2022-05-21 00:33:33 -0500

    reconfiguring init to include database for user authentication

---
+++ Down-The-Rabbit-Hole/rpack/main.py
@@ -0,0 +1,14 @@
+#TODO make this into its own package, that will also be used in the website
+
+'''
+THE PLAN
+---------------------
+- function takes user input as variable
+- input = a word, phrase or really anything that can be searched
+- function randomly selects site from preapproved list to scrape
+- get first two paragraphs or maybe 200 words?
+- run it through a loop so every x amount of words, it inserts a new line
+- randomly choose a word from the new paragraph 
+- return both the new word and the 2 paragraphs
+
+'''
