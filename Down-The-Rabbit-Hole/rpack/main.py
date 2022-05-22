09c8dab910e90810ad7a355dfeaa50a6524766f9
Mandy-cyber <92461362+PeriodPrideJa@users.noreply.github.com>
2022-05-22 11:07:38 -0500

    working on web scraper aspect of site

--- Down-The-Rabbit-Hole/rpack/main.py
+++ Down-The-Rabbit-Hole/rpack/main.py
@@ -1,4 +1,52 @@
-#TODO make this into its own package, that will also be used in the website
+from get_chrome_driver import GetChromeDriver
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import time
+import random
+
+# subthemes = ['feminist literature', 
+#             'queer love letters', 
+#             'top horror novels turned to movies',
+#             'top 10 fanfictions',
+#             'most tear-jerking poems']
+subtheme = 'top 10 fanfictions'
+
+def set_up():
+    get_driver = GetChromeDriver() 
+    get_driver.install()
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--log-level=3")
+    browser = webdriver.Chrome(options=options)
+    return browser
+
+def scrape_wiki(subtheme, browser):
+    browser.get("https://www.wikipedia.com")
+    searchbar = browser.find_element_by_name("search")
+    searchbar.send_keys(subtheme)
+    searchbar.send_keys(Keys.RETURN)
+
+# def scrape_citi(subtheme):
+#     print("")
+
+# def scrape_brit(subtheme):
+#     print("")
+
+
+
+# def make_selection(subtheme):
+#     websites = ['britannica', 'citizendium', 'wikipedia']
+#     website = random.choice(websites)
+#     if website == 'britannica':
+#         scrape_brit(subtheme)
+#     elif website == 'citizendium':
+#         scrape_citi(subtheme)
+#     else:
+#         scrape_wiki(subtheme)
+#     return
+
+
 
 '''
 THE PLAN
